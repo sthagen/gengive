4e4fd3cdaba334fd26c1c1ab193cc20e84ac56e7
Stefan Hagen <stefan@hagen.link>
2022-01-11 21:54:55 +0100

    Added some tests for implementation - coverage below 60% for implementation

---
+++ tests/test_gengive.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=line-too-long,missing-docstring,reimported,unused-import,unused-variable
+import pathlib
+
+import gengive.gengive as gg
+
+
+def test_gg_main():
+    inp = str(pathlib.Path('tests', 'fixtures', 'empty', 'advisory.json'))
+    assert gg.main(['verify', inp, '']) == 1
+
+
+def test_gg_verify_request_too_few():
+    assert gg.verify_request(['1']) == (2, 'received wrong number of arguments', [''])
+
+
+def test_gg_verify_request_unknown_command():
+    assert gg.verify_request(['unknown', 'later', 'does not matter']) == (2, 'received unknown command', [''])
+
+
+def test_gg_verify_request_falsy_input():
+    argv = ['verify', '', '']
+    assert gg.verify_request(argv) == (2, 'configuration missing', [''])
+
+
+def test_reader_empty():
+    inp = str(pathlib.Path('tests', '__init__.py'))
+    assert next(gg.reader(inp)).strip() == '# -*- coding: utf-8 -*-'
