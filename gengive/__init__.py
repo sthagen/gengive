c5262364d2403694c75b1955e9ed7dba87ab23ef
Stefan Hagen <stefan@hagen.link>
2022-08-02 06:14:51 +0200

    Bolted on the now usual init

--- gengive/__init__.py
+++ gengive/__init__.py
@@ -1,3 +1,23 @@
 """Render text (Danish: gengive tekst)."""
-__version__ = '2022.2.3'
-__version_info__ = tuple(__version__.split('.'))
+import os
+from typing import List
+
+APP_NAME = 'Render text (Danish: gengive tekst).'
+APP_ALIAS = 'gengive'
+APP_ENV = 'GENGIVE'
+DEBUG = bool(os.getenv(f'{APP_ENV}_DEBUG', ''))
+VERBOSE = bool(os.getenv(f'{APP_ENV}_VERBOSE', ''))
+QUIET = False
+STRICT = bool(os.getenv(f'{APP_ENV}_STRICT', ''))
+ENCODING = 'utf-8'
+ENCODING_ERRORS_POLICY = 'ignore'
+DEFAULT_CONFIG_NAME = '.gengive.json'
+DEFAULT_LF_ONLY = 'YES'
+
+# [[[fill git_describe()]]]
+__version__ = '2022.8.1+parent.7635e35c'
+# [[[end]]] (checksum: 336a0105003ee28994a568480df2fbd9)
+__version_info__ = tuple(
+    e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
+)
+__all__: List[str] = []
