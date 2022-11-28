646649d18edd97663a87e68cd8786eead204c1dc
Stefan Hagen <stefan@hagen.link>
2022-11-29 00:16:50 +0100

    Bumped impl version

--- gengive/__init__.py
+++ gengive/__init__.py
@@ -15,8 +15,8 @@ DEFAULT_CONFIG_NAME = '.gengive.json'
 DEFAULT_LF_ONLY = 'YES'
 
 # [[[fill git_describe()]]]
-__version__ = '2022.8.2+parent.976ad259'
-# [[[end]]] (checksum: c8cf91bc8a2a034e7c3ce5d04db22cf0)
+__version__ = '2022.11.28+parent.a11a1ad1'
+# [[[end]]] (checksum: 5ab1f5b5be679090479cca5ee795b198)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
