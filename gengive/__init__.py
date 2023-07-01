6b27bbac1e17dcd854e1ee80a77f126936fce754
Stefan Hagen <stefan@hagen.link>
2023-07-01 17:03:47 +0200

    Derived some app specific variable values from module (well file) docstring and the place

--- gengive/__init__.py
+++ gengive/__init__.py
@@ -1,17 +1,19 @@
 """Render text (Danish: gengive tekst)."""
 import os
+import pathlib
 from typing import List
 
-APP_NAME = 'Render text (Danish: gengive tekst).'
-APP_ALIAS = 'gengive'
-APP_ENV = 'GENGIVE'
+APP_ALIAS = str(pathlib.Path(__file__).parent.name)
+APP_ENV = APP_ALIAS.upper()
+APP_NAME = locals()['__doc__']
 DEBUG = bool(os.getenv(f'{APP_ENV}_DEBUG', ''))
 VERBOSE = bool(os.getenv(f'{APP_ENV}_VERBOSE', ''))
 QUIET = False
 STRICT = bool(os.getenv(f'{APP_ENV}_STRICT', ''))
 ENCODING = 'utf-8'
 ENCODING_ERRORS_POLICY = 'ignore'
-DEFAULT_CONFIG_NAME = '.gengive.json'
+DEFAULT_CONFIG_NAME = f'.{APP_ALIAS}.json'
+
 DEFAULT_LF_ONLY = 'YES'
 
 # [[[fill git_describe()]]]
